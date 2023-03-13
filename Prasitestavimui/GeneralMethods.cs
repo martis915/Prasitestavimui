0090645f5e530faaa19c3e575ba24589b7fdabfd
Work <Work@DESKTOP-57NLAU5>
2023-03-13 19:13:54 +0200

    uu

--- Prasitestavimui/GeneralMethods.cs
+++ Prasitestavimui/GeneralMethods.cs
@@ -17,10 +17,35 @@ namespace Prasitestavimui
         }
 
         public void ClickElementBy(string xpath) {
+            int tries = 0;
+            while (tries<5) {
+                try
+                {
+                    IWebElement el = driver.FindElement(By.XPath(xpath));
+                    IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                    js.ExecuteScript("arguments[0].scrollIntoView(true);", el);
+                    el.Click();
+                    break;
+                }
+                catch (Exception e )
+                {
+                    tries++;
+                    Console.WriteLine("Could not find element " + xpath +"\n"+ e);
+                    if (tries >= 3)
+                    {
+                        IJavaScriptExecutor javascriptExecutor = (IJavaScriptExecutor)driver;
+                        javascriptExecutor.ExecuteScript("arguments[0].click();",driver.FindElement(By.XPath(xpath)));
 
+                        Assert.Fail("Failed after " + tries + " Tries\n" + e);
+                    }
+                }
+            }
         }
 
-
+        public void EnterTextBy(string xpath, string text) {
+            By searchField = By.XPath(xpath);
+            driver.FindElement(searchField).SendKeys(text);
+        }
 
     }
 }

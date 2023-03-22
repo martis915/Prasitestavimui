a029126da8bb35df05a95679e0ed111696b6df54
Work <Work@DESKTOP-57NLAU5>
2023-03-22 18:55:27 +0200

    aaa

--- Prasitestavimui/GeneralMethods.cs
+++ Prasitestavimui/GeneralMethods.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Prasitestavimui
@@ -14,26 +16,21 @@ namespace Prasitestavimui
     internal class GeneralMethods
     {
         IWebDriver driver;
+        DefaultWait<IWebDriver> wait;
+
         public GeneralMethods(IWebDriver driver) { 
             this.driver = driver;
+            wait = new DefaultWait<IWebDriver>(driver);
+            wait.Timeout = TimeSpan.FromSeconds(10);
+            wait.PollingInterval = TimeSpan.FromMilliseconds(250);
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
         }
 
         public void ClickElementBy(string xpath) {
-
-            try { 
-            IWebElement el = driver.FindElement(By.XPath(xpath));
-            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-            js.ExecuteScript("arguments[0].scrollIntoView(true);", el);
-            driver.FindElement(By.XPath(xpath)).Click();
-            }
-            catch (Exception ex)
-            {
-                Thread.Sleep(1000);
-                IWebElement el = driver.FindElement(By.XPath(xpath));
-                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-                js.ExecuteScript("arguments[0].scrollIntoView(true);", el);
-                driver.FindElement(By.XPath(xpath)).Click();
-            }
+        
+            wait.Message = "Cookie accept button was not found";
+            IWebElement elm = wait.Until(x => x.FindElement(By.XPath(xpath)));
+            elm.Click();
 
         }
 
@@ -46,8 +43,10 @@ namespace Prasitestavimui
         }
 
         public void EnterTextBy(string xpath, string text) {
-            By searchField = By.XPath(xpath);
-            driver.FindElement(searchField).SendKeys(text);
+          
+            wait.Message = "Cookie accept button was not found";
+            IWebElement elm = wait.Until(x => x.FindElement(By.XPath(xpath)));
+            elm.SendKeys(text);
         }
 
 
@@ -62,6 +61,12 @@ namespace Prasitestavimui
         public void CheckElementExistsByID(string id)
         {
             driver.FindElement(By.Id(id));
+        }
+
+        public string getUsername() {
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Work\Documents\Projects\Prasitestavimui\Prasitestavimui\LoginDetails.txt");
+            return lines[1];
         }
 
 
--- Prasitestavimui/Tests/OtherCases.cs
+++ Prasitestavimui/Tests/OtherCases.cs
@@ -9,6 +9,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using OpenQA.Selenium.DevTools.V108.CSS;
 using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Interactions;
 using Prasitestavimui.Generators;
 using System.Xml.Linq;
@@ -20,6 +23,7 @@ namespace Prasitestavimui
     {
         static IWebDriver driver;
         static GeneralMethods generalMethods;
+
         [SetUp]
         public static void SETUP()
         {
@@ -28,31 +32,26 @@ namespace Prasitestavimui
             driver = new ChromeDriver(options);
             driver.Manage().Window.Maximize();
             driver.Url = "https://www.1a.lt";
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-
             generalMethods = new GeneralMethods(driver);
-            generalMethods.ClickElementBy("//a[@class=\"c-button\"]");
 
 
         }
         [TearDown]
         public static void TearDown()
         {
-            driver.Quit();
+            //driver.Quit();
         }
         [Test]
         public static void CheckProductCardElements()
         {
-            TopMenu topMenu = new TopMenu(driver);
-            ProductList productList = new ProductList(driver);
-            ProductCard productCard = new ProductCard(driver);
+            DefaultWait<IWebDriver> wait = new DefaultWait<IWebDriver>(driver);
+            wait.Timeout = TimeSpan.FromSeconds(10);
+            wait.PollingInterval = TimeSpan.FromMilliseconds(250);
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            wait.Message = "LAbas";
+            IWebElement elm = wait.Until(x => x.FindElement(By.XPath("//AA")));
+            elm.Click();
 
-            topMenu.SearchByText("iphone 13");
-            productList.OpenFirstProduct();
-            productCard.CheckBreadcrumbsCount();
-            generalMethods.CheckElementExistsByID("add_to_cart_btn");
-            generalMethods.CheckElementExistsByID("description-anchor");
-            generalMethods.CheckElementExistsByID("also-bought-vue-wrapper");
         }
 
 
